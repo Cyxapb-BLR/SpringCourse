242552ec8a43d2c0c375dd147b175c80b7120fb1
Cyxapb-BLR <cyxapb-511@yandex.ru>
2021-05-05 14:25:17 -0400

    lesson11, @Qualifier annotation

--- src/main/java/com/matskevich/springcource/MusicPlayer.java
+++ src/main/java/com/matskevich/springcource/MusicPlayer.java
@@ -1,20 +1,38 @@
 package com.matskevich.springcource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+    /*@Autowired
+    @Qualifier("classicalMusic")
+    private Music music;*/
+    private Music music1;
+    private Music music2;
+
+    @Autowired
+    public MusicPlayer(
+            @Qualifier("classicalMusic") Music music1,
+            @Qualifier("musicBean") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
+    }
+
+    public String playMusic() {
+        return "Playing: " + music1.getSong() + ". Next song : " + music2.getSong();
+        // System.out.println("Playing: " + rockMusic.getSong());
+    }
     /* @Autowired
      private Music music;*/
-    private ClassicalMusic classicalMusic;
+    /*private ClassicalMusic classicalMusic;
     private RockMusic rockMusic;
-
     @Autowired
     public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
         this.classicalMusic = classicalMusic;
         this.rockMusic = rockMusic;
-    }
+    }*/
 /* private List<Music> musicList = new ArrayList<>();
      private String name;
      private int volume;*/
@@ -27,11 +45,6 @@ public class MusicPlayer {
     public void setMusic(Music music) {
         this.music = music;
     }*/
-
-    public String playMusic() {
-       return "Playing: " + classicalMusic.getSong();
-       // System.out.println("Playing: " + rockMusic.getSong());
-    }
 
 //    public void setMusicList(List<Music> musicList) {
 //        this.musicList = musicList;
